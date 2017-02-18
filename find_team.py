810ae1df7f9f58c95bee579a6e8926df1df2bff1
raphael <raph.koh@gmail.com>
2017-02-18 16:10:28 -0500

    Changes to find_team

--- find_team.py
+++ find_team.py
@@ -17,13 +17,13 @@ def new_user(username, name, roles, skills):
                                "name": name, 
                                "roles": roles, 
                                "skills": skills}
-                               
+
     with open('data.txt', 'w') as outfile:
         json.dumps(looking_users, outfile)
 
 def load_data(database_name):
     """Loads data from database"""
-    
+    global looking_users
     with open(database_name, 'r') as infile:
         looking_users = json.load(infile)
         
@@ -32,5 +32,18 @@ def get_user(username):
     
     Keyword Arguments:
     username -- user's username as a string"""
-    
+
     return looking_users["users"][username]
+
+def filter_role(roles):
+    if not isinstance(roles, list):
+        print ("Invalid Input!")
+        return
+    load_data("find_team_users.txt")
+    data = list(map(lambda x: score_user(roles, x["roles"]), looking_users["users"]))
+    print(data)
+
+#scores users based on how many matches
+def score_user(roles, user_roles):
+    score = reduce(lambda x,y: (y + 1) if (x in roles) else y, user_roles, 0)
+    return score
